1e866b9acb328f1aaea3d83a83dfa58cb2698fab
ttmdear <ttmdear@gmail.com>
2018-08-26 16:24:12 +0200

    General changes at Dialog.

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -1,5 +1,8 @@
 import TopiObject from 'Topi/Object';
+
 import ViewDialog from 'Topi/Dialog/Views/Dialog';
+import ViewAlert from 'Topi/Dialog/Views/Alert';
+import ViewModal from 'Topi/Dialog/Views/Modal';
 
 const cn = 'Dialog';
 class Dialog extends TopiObject {
@@ -13,6 +16,39 @@ class Dialog extends TopiObject {
         app.target().append(view.element());
 
         return view;
+    }
+
+    /**
+     * Display alert.
+     */
+    alert(params = {}) {
+        const app = this.component('@app'),
+            view = new ViewAlert(params)
+        ;
+
+        view.render();
+
+        app.target().append(view.element());
+
+        return view;
+    }
+
+    /**
+     * Modal
+     */
+    modal(view, params = {}) {
+        const app = this.component('@app'),
+            modal = new ViewModal(view)
+        ;
+
+        modal
+            .target(app.target())
+        ;
+
+        app.target().append(modal.element());
+        modal.render();
+
+        return modal;
     }
 }
 
